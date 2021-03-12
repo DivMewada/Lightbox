cddf68c367d27b16fd703c48d56a0c4e95495848
Divya Suthar <divyasuthar1605@gmail.com>
2021-03-12 15:49:47 +0530

    Replace image loading library Imaginary with SDWebImage to suppprt GIF

--- Source/LightboxConfig.swift
+++ Source/LightboxConfig.swift
@@ -1,7 +1,7 @@
 import UIKit
 import AVKit
 import AVFoundation
-import Imaginary
+import SDWebImage
 
 public class LightboxConfig {
   /// Whether to show status bar while Lightbox is presented
@@ -21,14 +21,14 @@ public class LightboxConfig {
   public static var loadImage: (UIImageView, URL, ((UIImage?) -> Void)?) -> Void = { (imageView, imageURL, completion) in
 
     // Use Imaginary by default
-    imageView.setImage(url: imageURL, placeholder: nil, completion: { result in
-      switch result {
-      case .value(let image):
-        completion?(image)
-      case .error:
+    imageView.sd_setImage(with: imageURL) { (image, error, cacheType, url) in
+      if error != nil {
         completion?(nil)
       }
-    })
+      if image != nil {
+        completion?(image)
+      }
+    }
   }
 
   /// Indicator is used to show while image is being fetched
